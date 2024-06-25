edce57b4a26a764166bd73630c5d950a7aecbe59
TrungAzieL <trung.nguyenminh7804@gmail.com>
2024-06-25 13:16:08 +0700

    add: Image Creator API

---
+++ create_image_with_API.py
@@ -0,0 +1,20 @@
+import replicate
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = 'r8_HACwu6uvphc0ARNU1HDkw5CqJXW4mME2MYkoX' # Yout APT token
+# https://replicate.com/        <--  Test it hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+output = replicate.run(
+    "bytedance/sdxl-lightning-4step:5f24084160c9089501c1b3545d9be3c27883ae2239b6f412990e82d4a6210f8f",
+    input={
+        "width": 1024,
+        "height": 1024,
+        "prompt": "self-portrait of a girl walking under the rainbow, anime style",
+        "scheduler": "K_EULER",
+        "num_outputs": 1,
+        "guidance_scale": 0,
+        "negative_prompt": "worst quality, low quality",
+        "num_inference_steps": 4
+    }
+)
+print(output)
